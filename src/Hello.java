4feb3f99d3a8709920b7157f94c2414107729759
sumitthakurtech <sumitthakur5044@gmail.com>
2024-01-20 01:05:31 +0530

    HelloJava

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,23 @@
 // Classic Hello World  Program #Nostalgia
  class Hello{
 public static void main(String[]args){
-    System.out.println("Hello World!");  // can't miss the colon :)
+    System.out.println("Hello World!");  
+// Primitive Data Type 
+/*
+ 1. integer - > Byte,short,int,long (1,2,4,8bytes)
+ 2. Float -> float ,double (4,8) (f inend for float)
+ 3. Char -> (1)
+ 4.Boolean
+ */
+byte age = 127;
+short run = 32767 ; 
+int marks = 2147483647;
+long views = 9_223_372_036_854_775_807L;
+System.out.println(age+"\n"+run+"\n"+marks+"\n"+views+"\n");
+// long views= 9,223,372,036,854,775,800; 
+
+// System.out.println(views);
+
+
 }
 }
